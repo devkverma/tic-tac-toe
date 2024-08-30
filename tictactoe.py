94413713213a459d3404dd262872366926d3fd9e
devkverma <devverma269@gmail.com>
2024-08-30 13:30:58 +0530

    Added constants.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,2 +1,20 @@
 import sys
 import pygame
+from constants import *
+
+# Pygame
+pygame.init()
+screen = pygame.display.set_mode( (WIDTH,HEIGHT))
+pygame.display.set_caption('TIC TAC TOE')
+
+def main():
+
+    while True:
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+main()
