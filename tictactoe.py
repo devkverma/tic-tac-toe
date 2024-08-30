34319d73a9f4ab1b5bc0ffce5b71e9ee1d51258f
devkverma <devverma269@gmail.com>
2024-08-30 19:05:47 +0530

    Updated tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import numpy as np
 from constants import *
 
 # PYGAME SETUP
@@ -8,16 +9,42 @@ screen = pygame.display.set_mode( (WIDTH,HEIGHT))
 pygame.display.set_caption('TIC TAC TOE')
 screen.fill(BG_COLOR)
 
+class Board:
+    def __init__(self):
+        self.squares = np.zeros( (ROWS,COLS) )
+    
+    def mark_sqr(self,row,col,player):
+        self.squares[row][col] = player
+
+    def empty_sqr(self,row,col):
+        return self.squares[row][col] == 0
+
 class Game:
 
     def __init__(self):
-        pass
+        self.board = Board()
+        self.player = 1
+        self.show_lines()
 
     def show_lines(self):
-        pass
+        # vertical
+        pygame.draw.line(screen,LINE_COLOR,(SQSIZE,0),(SQSIZE,HEIGHT),LINE_WIDTH)
+        pygame.draw.line(screen,LINE_COLOR,(WIDTH - SQSIZE,0),(WIDTH - SQSIZE,HEIGHT),LINE_WIDTH)
+        
+        # horizontal
+        pygame.draw.line(screen,LINE_COLOR,(0,SQSIZE),(WIDTH,SQSIZE),LINE_WIDTH)
+        pygame.draw.line(screen,LINE_COLOR,(0,HEIGHT - SQSIZE),(WIDTH,HEIGHT - SQSIZE),LINE_WIDTH)
+
+    def next_turn(self):
+        self.player = self.player % 2 + 1
 
 def main():
 
+    # object
+    game = Game()
+    board = game.board
+
+    # mainloop
     while True:
 
         for event in pygame.event.get():
@@ -25,6 +52,20 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = event.pos
+                row = pos[1] // SQSIZE
+                col = pos[0] // SQSIZE
+
+                if board.empty_sqr(row,col):
+                    board.mark_sqr(row,col,game.player)
+                    game.draw_fig(row,col)
+                    game.next_turn()
+                    
+                    
+                
+                
         
         pygame.display.update()
 
